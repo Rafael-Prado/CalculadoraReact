2fedede1aa49e8c9d1b08d2cb4f93060ac2f6d72
Rafael-Prado <rafaeldacruzdoprado@hotmail.com>
2019-05-09 09:42:21 -0300

    Iniciando o projeto calculadoraReact

---
+++ src/main/Caculator.jsx
@@ -0,0 +1,12 @@
+import React , { Componet } from 'react';
+import './Calculator.css'
+
+export default class Calculator extends Componet {
+    reder () {
+        return (
+            <div>   
+                
+            </div>
+        )
+    }
+}
